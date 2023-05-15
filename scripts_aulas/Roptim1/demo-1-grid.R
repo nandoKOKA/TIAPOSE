254662410f24376a216624098ac9239d8d92e6ed
nandoKOKA <97353901+nandoKOKA@users.noreply.github.com>
2023-05-15 17:32:53 +0100

    1

--- scripts_aulas/Roptim1/demo-1-grid.R
+++ scripts_aulas/Roptim1/demo-1-grid.R
@@ -5,12 +5,12 @@ source("grid.R") #  gsearch is defined here
 sphere=function(x) sum(x^2)
 
 # 1st example: dimension D=2
-D=2
-lower=rep(-10.4,D) # lower bounds
-upper=rep(10.4,D) #  upper bounds
+D=7
+lower=rep(0,D) # lower bounds
+upper=rep(15,D) #  upper bounds
 # grid search 10x10 search: length(seq(-10.4,10.4,by=2.1))== 10:
 range=upper[1]-lower[1]
-nsearches=10 # searches per dimension
+nsearches=20 # searches per dimension
 jump=range/(nsearches-1) # 10 searches per dimension
 step=rep(jump,D) # 10 searches per dimension
 iter=nsearches^D
@@ -18,7 +18,7 @@ cat("grid search sphere D=",D,"step=",step,"(iters=",iter,")\n")
 GS=gsearch(fn=sphere,lower=lower,upper=upper,step=step,type="min")
 cat("best solution:",GS$sol,"evaluation function",GS$eval," (found at iteration:",GS$index,")\n")
 
-# 2nd example: dimension D=5
+#2nd example: dimension D=5
 D=5
 lower=rep(-10.4,D) # lower bounds
 upper=rep(10.4,D) #  upper bounds
