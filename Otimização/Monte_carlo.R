9a040f22cf75e494644ce5422f7b846c473df7e4
onlymr <73614181+onlymr@users.noreply.github.com>
2023-05-16 22:47:18 +0100

    last

--- Otimização/Monte_carlo.R
+++ Otimização/Monte_carlo.R
@@ -28,12 +28,12 @@ N=10000 # number of searches
 
 montecarlo <- function(lower, upper, N) {
   # Create the vectors
-  arm <- c(0, 0, 0, 0, 0, 0)
-  v1 <- c(0, 0, 0, 0, 0, 0)
-  v2 <- c(0, 0, 0, 0, 0, 0)
-  v3 <- c(0, 0, 0, 0, 0, 0)
-  pac_stella <- c(0, 0, 0, 0, 0, 0)
-  pac_bud <- c(0, 0, 0, 0, 0, 0)
+  arm <- c(0, 0, 0, 0, 0, 0,0)
+  v1 <- c(0, 0, 0, 0, 0, 0,0)
+  v2 <- c(0, 0, 0, 0, 0, 0,0)
+  v3 <- c(0, 0, 0, 0, 0, 0,0)
+  pac_stella <- c(0, 0, 0, 0, 0, 0,0)
+  pac_bud <- c(0, 0, 0, 0, 0, 0,0)
   
   
   # evaluation function:
@@ -55,18 +55,49 @@ montecarlo <- function(lower, upper, N) {
 
 
 # Verificar se as restrições são satisfeitas
-verifica <- function(s) {
-    arm <- s[1:6]
-    pac_stella <- s[29:35]
-    pac_bud <- s[36:42]
+repair <- function(s) {
+  s = round(s)
+  
+  arm <- s[1:7]
+  v1 <- s[8:14]
+  v2 <- s[15:21]
+  v3 <- s[22:28]
+  pac_stella <- s[29:35]
+  pac_bud <- s[36:42]
   
   for (i in 1:7) {
-    if (pac_stella[i] > max_dist_v1 || pac_stella[i] > max_dist_v2 || pac_stella[i] > max_dist_v3 || arm[i] > max_pac_rec || pac_bud[i] > max_dist_v1 || pac_bud[i] > max_dist_v2 || pac_bud[i] > max_dist_v3) {
-      return(-Inf)
-    } else {
-      return(s)
+    ## tratar veículos e bebidas empacotadas
+    
+    if (pac_stella[i] > (arm[i] * max_pac_rec) ||
+        pac_bud[i] > (arm[i] * max_pac_rec))
+    {
+      pac_stella[i] <- pac_stella[i] / max_pac_rec
+      pac_bud[i] <- pac_bud[i] / max_pac_rec
+    } 
+    
+    if ((pac_stella[i] + pac_bud[i]) > ((v1[i] * max_dist_v1) + (v2[i] * max_dist_v2) + (v3[i] * max_dist_v3))) {
+      resto <- (pac_stella[i] + pac_bud[i]) - ((v1[i] * max_dist_v1) + (v2[i] * max_dist_v2) + (v3[i] * max_dist_v3))
+      
+      while (resto > 0) {
+        if (resto < 60) {
+          v1[i] <- v1[i] + 1
+          resto <- resto - 60
+        } else if (resto < 90) {
+          v2[i] <- v2[i] + 1
+          resto <- resto - 90
+        } else if (resto < 120) {
+          v3[i] <- v3[i] + 1
+          resto <- resto - 120
+        } else {
+          v3[i] <- v3[i] + 1
+          resto <- resto - 120
+        }
+      }
     }
   }
+  
+  s1 <- c(arm, v1, v2, v3, pac_stella, pac_bud)
+  return(s1)
 }
 
 s1 <- montecarlo(lower, upper, N)
--- Otimização/Monte_carlo_blind.R
+++ Otimização/Monte_carlo_blind.R
@@ -51,46 +51,8 @@ montecarlo <- function(lower, upper, N) {
 
 # Verificar se as restrições são satisfeitas
 repair <- function(s) {
-  s=round(s)
+  s = round(s)
   
-  for (i in 1:7) {
-    ## tratar veiculos e bebidas empacotadas
-    
-    if (
-      pac_stella[i] > (arm[i] * max_pac_rec) ||
-      pac_bud[i] > (arm[i] * max_pac_rec))
-      {
-      pac_stella[i] = pac_stella[i] / max_pac_rec
-      pac_bud[i] = pac_bud[i] / max_pac_rec
-      
-    } else if ((pac_stella[i] + pac_bud[i]) > ((v1[i]*max_dist_v1)  + (v2[i]*max_dist_v2) + (v3[i]*max_dist_v3)) ) {
-      
-       resto <- (pac_stella[i] + pac_bud[i]) - ((v1[i]*max_dist_v1)  + (v2[i]*max_dist_v2) + (v3[i]*max_dist_v3))
-       
-       while(resto != 0){
-         
-           if(resto <60){
-             v1[i] = v1[i] + 1
-             resto = resto - 60
-             
-           } else if (resto<90) {
-             v2[i] = v2[i] + 1
-             resto= restp - 90
-             
-           } else if(resto<120) {
-             v3[i] = v[3] + 1
-             resto= resto - 90
-           }
-           
-          else { ## quando é maior que 120 adiciona logo um v3
-              v3[i] = v3[i] + 1
-              resto= resto - 120  #atualizar o resto
-          }
-    }
-    
-  }
-      
-  }
   arm <- s[1:7]
   v1 <- s[8:14]
   v2 <- s[15:21]
@@ -98,9 +60,39 @@ repair <- function(s) {
   pac_stella <- s[29:35]
   pac_bud <- s[36:42]
   
+  for (i in 1:7) {
+    ## tratar veículos e bebidas empacotadas
+    
+    if (pac_stella[i] > (arm[i] * max_pac_rec) ||
+        pac_bud[i] > (arm[i] * max_pac_rec))
+    {
+      pac_stella[i] <- pac_stella[i] / max_pac_rec
+      pac_bud[i] <- pac_bud[i] / max_pac_rec
+    } 
+    
+    if ((pac_stella[i] + pac_bud[i]) > ((v1[i] * max_dist_v1) + (v2[i] * max_dist_v2) + (v3[i] * max_dist_v3))) {
+      resto <- (pac_stella[i] + pac_bud[i]) - ((v1[i] * max_dist_v1) + (v2[i] * max_dist_v2) + (v3[i] * max_dist_v3))
+      
+      while (resto > 0) {
+        if (resto < 60) {
+          v1[i] <- v1[i] + 1
+          resto <- resto - 60
+        } else if (resto < 90) {
+          v2[i] <- v2[i] + 1
+          resto <- resto - 90
+        } else if (resto < 120) {
+          v3[i] <- v3[i] + 1
+          resto <- resto - 120
+        } else {
+          v3[i] <- v3[i] + 1
+          resto <- resto - 120
+        }
+      }
+    }
+  }
+  
   s1 <- c(arm, v1, v2, v3, pac_stella, pac_bud)
-  #print(s1)
-  #return(s1)
+  return(s1)
 }  
 
 s1 <- montecarlo(lower, upper, N)
